e9a61f2b07500b716eafbb0fec9f46adbe5a4e9a
Lucas Correia <lucascorreiamelo12@gmail.com>
2026-03-30 13:37:18 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,21 @@
+# inteiro = int
+# ponto flutuante (números com casas decimais) = float
+# string (palavra) = str
+# booleano = true ou false = bool
+
+#exemplo de variaveis
+idade = 21 # inteiro
+altura = 1.67 # ponto flutuante
+nome = "Luke" # string
+estudando = True # booleano
+
+print("Idade:", idade)
+print("Altura:", altura)
+print("Nome:", nome)
+print("Estudando:", estudando)
+
+# Pode criar qualquer nome para variavel, mas deve seguir algumas regras:
+# - Deve começar com letra ou underscore (_)
+# - Pode conter letras, numeros e underscores
+# - Nao pode conter espacos
+# - Nao pode ser uma palavra reservada do Python (como print, if, else, etc)
